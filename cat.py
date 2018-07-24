d66f6c8c3269b1c83a7938a3980091c940135bde
HanmenChan <41607329+HanmenChan@users.noreply.github.com>
2018-07-24 21:29:36 +0800

    Combine cells
    融合很多个cells

--- cat.py
+++ cat.py
@@ -1,13 +1,24 @@
 import json
 import sys
 
-#accept argument 接受变量
-notebook_path1 = sys.argv[1]
-notebook_path2 = sys.argv[2]
+#accept argument list 接受变量
+notebook_path_lst = sys.argv[1:]
+
+cells_lst = []
+target_notebook = {}
+#读取 path list
+
+for path in notebook_path_lst:
+    notebook = open(path)
+    notebook_str = notebook.read()
+    notebook_json = json.loads(notebook_str)
+    cells = notebook_json['cells']
+    cells_lst += (cells)
+target_notebook['cells'] = cells_lst
 
 
 
-#读取
+'''
 notebook1 = open(notebook_path1)
 notebook1_str = notebook1.read()
 notebook1_json = json.loads(notebook1_str)
@@ -24,7 +35,7 @@ del notebook1_json['cells']
 
 
 #拼接
-target_cells = cells1 + cells2
+target_cells  cells1 + cells2
 
 target_notebook = {}
 
@@ -35,3 +46,4 @@ target_str = json.dumps(target_notebook)
 
 target = open('target_notebook.ipynb','w')
 target.write(target_str)
+'''
