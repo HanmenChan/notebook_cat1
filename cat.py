7d0267afb8553977d5bece714decd04cf8710f81
HanmenChan <41607329+HanmenChan@users.noreply.github.com>
2018-07-24 19:58:49 +0800

    Complete JSON write
    使用JSON完成读写

--- cat.py
+++ cat.py
@@ -5,19 +5,20 @@ import json
 #读取
 notebook1 = open('1.ipynb')
 notebook1_str = notebook1.read()
-
 notebook1_json = json.loads(notebook1_str)
-print(notebook1_json)
 
 cells1 = notebook1_json['cells']
 
 notebook2 = open('2.ipynb')
 notebook2_str = notebook2.read()
-
 notebook2_json = json.loads(notebook2_str)
-print(notebook2_json)
 
 cells2 = notebook2_json['cells']
 
 #拼接
 cells = cells1 + cells2
+
+cells_json = json.dumps(cells)
+
+target = open('target_notebook.ipynb','w')
+target.write(cells_json)
