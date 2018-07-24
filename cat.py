29f68d57b19126d78372aa34874dadea717e8808
HanmenChan <41607329+HanmenChan@users.noreply.github.com>
2018-07-24 21:58:35 +0800

    Catcatcat

--- cat.py
+++ cat.py
@@ -1,6 +1,10 @@
 import json
 import sys
 
+#handle argument exceptions 抛出异常
+if len(sys.argv) < 3:
+    raise Exception('参数数量必须大于 2！')
+
 #accept argument list 接受变量
 notebook_path_lst = sys.argv[1:]
 
@@ -9,17 +13,18 @@ target_notebook = {}
 #读取 path list
 
 for path in notebook_path_lst:
-    notebook = open(path)
-    notebook_str = notebook.read()
-    notebook_json = json.loads(notebook_str)
-    cells = notebook_json['cells']
-    del notebook_json['cells']
-    cells_lst += (cells)
+    with open(path) as notebook:
+        notebook_str = notebook.read()
+        notebook_json = json.loads(notebook_str)
+        cells = notebook_json['cells']
+        del notebook_json['cells']
+        cells_lst += (cells)
+
 target_notebook['cells'] = cells_lst
 target_notebook.update(notebook_json)
 target_str = json.dumps(target_notebook)
-target = open('target_notebook.ipynb','w')
-target.write(target_str)
+with open('target_notebook.ipynb','w') as target:
+    target.write(target_str)
 
 
 '''
