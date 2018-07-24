f972c0f758dbd29b880e647503b96ecf3240727c
HanmenChan <41607329+HanmenChan@users.noreply.github.com>
2018-07-24 19:48:02 +0800

    read
    读取文件

--- cat.py
+++ cat.py
@@ -1,11 +1,23 @@
+import json
+
+
+
 #读取
-notebook1 = {}
+notebook1 = open('1.ipynb')
+notebook1_str = notebook1.read()
 
-cells1 = notebook1['cells']
+notebook1_json = json.loads(notebook1_str)
+print(notebook1_json)
 
-notebook2 = {}
+cells1 = notebook1_json['cells']
 
-cells2 = notebook2['cells']
+notebook2 = open('2.ipynb')
+notebook2_str = notebook2.read()
+
+notebook2_json = json.loads(notebook2_str)
+print(notebook2_json)
+
+cells2 = notebook2_json['cells']
 
 #拼接
 cells = cells1 + cells2
